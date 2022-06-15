c54d04c99ace9a4449bee0d0dd72f942507c4a12
費聿暄 <stanfei@DESKTOP-2FLU8FE.localdomain>
2022-06-15 12:24:00 +0800

    dz60

--- keyboards/dz60/config.h
+++ keyboards/dz60/config.h
@@ -3,11 +3,11 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x445A  // "DZ"
-#define PRODUCT_ID      0x2260
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    KBDFans
-#define PRODUCT         DZ60
+#define VENDOR_ID 0x445A // "DZ"
+#define PRODUCT_ID 0x2260
+#define DEVICE_VER 0x0001
+#define MANUFACTURER KBDFans
+#define PRODUCT DZ60
 
 /* key matrix size */
 #define MATRIX_ROWS 5
@@ -23,18 +23,20 @@
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D1, D2, D3, D5 }
-#define MATRIX_COL_PINS { F0, F1, E6, C7, C6, B7, D4, B1, B0, B5, B4, D7, D6, B3, F4 }
+#define MATRIX_ROW_PINS \
+    { D0, D1, D2, D3, D5 }
+#define MATRIX_COL_PINS \
+    { F0, F1, E6, C7, C6, B7, D4, B1, B0, B5, B4, D7, D6, B3, F4 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
 #define LED_CAPS_LOCK_PIN B2
-#define LED_PIN_ON_STATE 0
+// #define LED_PIN_ON_STATE 0
 
 #define BACKLIGHT_PIN B6
-#define BACKLIGHT_LEVELS 5
+// #define BACKLIGHT_LEVELS 5
 
 #define RGB_DI_PIN E2
 #ifdef RGB_DI_PIN
@@ -55,4 +57,4 @@
 #define LOCKING_RESYNC_ENABLE
 
 /* VIA related config */
-#define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
+// #define VIA_EEPROM_LAYOUT_OPTIONS_SIZE 2
---
+++ keyboards/dz60/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID \
+    { 0xD5, 0x34, 0x22, 0x3A, 0xD4, 0x0D, 0xF1, 0x99 }
+#define VIAL_UNLOCK_COMBO_ROWS \
+    { 0, 4 }
+#define VIAL_UNLOCK_COMBO_COLS \
+    { 0, 0 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
